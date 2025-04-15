2c6d442c2896fbe1a04cc45ab793882a9f6e5aa3
Ben Hockley <hockleyb@cardiff.ac.uk>
2025-04-15 16:14:52 +0100

    basic jinja2 templating

--- data/nba.py
+++ data/nba.py
@@ -4,10 +4,11 @@ from keys import API_KEY
 
 api = BalldontlieAPI(api_key=API_KEY)
 
-def get_nba_games():
+def get_nba_games(date : str):
     """
     Fetches the list of NBA games for the 2024 season.
     """
     # Fetching games for the 2024 season
-    games = api.nba.games.list(seasons=[2024]).data
+    # Dates in the format YYYY-MM-DD
+    games = api.nba.games.list(dates=[date]).data
     return games
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
 
+from datetime import datetime
+
 from data.nba import get_nba_games
 
 
@@ -19,9 +21,17 @@ async def root():
 
 @app.get("/home", response_class=HTMLResponse)
 async def home(request: Request):  # Include the Request object
-    games = get_nba_games()
-    return templates.TemplateResponse("home.html", {"request": request, "games": games})  # Render the home.html template
+    return templates.TemplateResponse("home.html", {"request": request})  # Render the home.html template
 
+@app.get("/redirectGames")
+async def redirectGames():
+    dateToday: str = datetime.today().strftime("%Y-%m-%d")
+    return RedirectResponse(url=f"/games/{dateToday}")
+
+@app.get("/games/{date}", response_class=HTMLResponse)
+async def games(request: Request, date: str):
+    games = get_nba_games(date)
+    return templates.TemplateResponse("games.html", {"request": request, "games": games})
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
