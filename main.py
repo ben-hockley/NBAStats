8b2d6b519f7eecd2ce409b13e95aa8c43f645d88
Ben Hockley <hockleyb@cardiff.ac.uk>
2025-04-15 12:04:00 +0100

    basic homepage loads on launch

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from fastapi import FastAPI # import FastAPI
-import uvicorn # import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
 
 
-app = FastAPI() # create an instance of FastAPI
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/") # path operator decorator
-async def root(): # path operator function
-    return {"message": "Hello World"}
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/home")
 
-@app.get("/items/{item_id}") # path operator decorator
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/home", response_class=HTMLResponse)
+async def home(request: Request):  # Include the Request object
+    return templates.TemplateResponse("home.html", {"request": request})  # Render the home.html template
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
